2b07113a17a0ff9de59b0b0061ace8ebdc16f5b9
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-09-07 18:00:37 +0100

    Create Game Logic: - A shuffle method to shuffle the cards - Removed Cryptos , loading, & error states from the Main to APP to handle - Write triggerSelect method to pass Card crypto id to the App - Handle Card click in the APp, manage CurrentScore, bestScore, scores state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,80 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import Main from "./components/Main.jsx";
+import { shuffle } from "../utilities/utility.js";
 import "./styles/general.css";
 import "./styles/header.css";
 import "./styles/main.css";
-import Main from "./components/Main.jsx";
 
 function App() {
+  const [cryptos, setCryptos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const [selectedCryptos, setSelectedCryptos] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [scores, setScores] = useState([0]);
+  const [bestScore, setBestScore] = useState(Math.max(...scores, currentScore));
+
+  // fetch crypto data
+  useEffect(() => {
+    const PROXY_URL = "/coingecko-api";
+    const API_KEY = import.meta.env.VITE_MEMORY_API_KEY;
+
+    fetch(
+      `${PROXY_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=12&page=1`,
+      {
+        headers: {
+          accept: "application/json",
+          "x-cg-demo-api-key": API_KEY,
+        },
+      },
+    )
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        return response.json();
+      })
+      .then((data) => setCryptos(data))
+      .catch((err) => {
+        console.error("Error fetching images:", err);
+        setError(err);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  // create method to shuffle the crypto array state
+  const shuffleCards = () => {
+    setCryptos((prevCryptos) => shuffle([...prevCryptos]));
+  };
+
+  const handleCardClick = (cryptoId) => {
+    if (selectedCryptos.includes(cryptoId)) {
+      // game Over!
+      setScores((prevScores) => [...prevScores, currentScore]);
+      setBestScore((prevBestScore) => Math.max(...scores, currentScore));
+      setCurrentScore(0);
+      setSelectedCryptos([]);
+    } else {
+      // correct Choice!
+      setSelectedCryptos((prevSelected) => [...prevSelected, cryptoId]);
+      setCurrentScore((prevScore) => prevScore + 1);
+    }
+
+    // reshuffle the cards
+    shuffleCards();
+  };
+
   return (
     <>
-      <Header score={2} bestScore={4} />
-      <Main />
+      <Header score={currentScore} bestScore={bestScore} />
+      <Main
+        cryptos={cryptos}
+        loading={loading}
+        error={error}
+        onCardSelect={handleCardClick}
+      />
       <Footer />
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,11 @@
-function Card({ imageUrl, imageAlt, title }) {
+function Card({ id, imageUrl, imageAlt, title, onSelect }) {
+  //
+  const triggerSelect = (e) => {
+    // pass the id of the crypto clicked upwards
+    onSelect(id);
+  };
   return (
-    <figure className="card">
+    <figure className="card" role="button" onClick={triggerSelect}>
       <img src={imageUrl} alt={imageAlt} />
       <figcaption className="title">{title}</figcaption>
     </figure>
